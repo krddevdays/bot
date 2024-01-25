5432e2a83c8200eb61521c5dd55e29e59c128707
agorozhanin <a.gorozhanin@vanguardsoft.ru>
2024-01-25 22:53:08 +0300

    fix markdown and user without usname

---
+++ krddevbot/service.py
@@ -0,0 +1,19 @@
+from typing import Union
+from telegram import User
+
+
+def get_user_name(user: Union[User, dict]) -> str:
+    if not isinstance(user, User):
+        user = User(
+            id=user.get('id'),
+            first_name=user.get('first_name'),
+            is_bot=user.get('is_bot'),
+            username=user.get('username')
+        )
+
+    username = user.username
+    if username:
+        username = username.replace('_', '\\_')
+        return f"@{username}"
+    else:
+        return user.mention_markdown_v2(user.first_name)
