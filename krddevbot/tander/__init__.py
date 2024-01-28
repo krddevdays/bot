347bc5350d9ecd9764382d97c87ad162a61c15dc
TyVik <tyvik8@gmail.com>
2024-01-28 12:55:06 +0300

    Move out tander handler.

--- krddevbot/tander/__init__.py
+++ krddevbot/tander/__init__.py
@@ -1,3 +1,4 @@
+import logging
 import pathlib
 import re
 from datetime import datetime, UTC
@@ -5,17 +6,21 @@ from datetime import datetime, UTC
 from telegram import Update
 from telegram.ext import ContextTypes
 
-
+logger = logging.getLogger(__name__)
 pattern = re.compile("тандер", re.IGNORECASE | re.MULTILINE | re.UNICODE)
 
 
 async def days_without_mention(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if pattern.search(update.message.text):
-        file_path = pathlib.Path(f'tander/{abs(update.effective_chat.id)}')
+        chat = update.effective_chat
+        logger.debug("В группе %s (%s) вспомнили Тандер", chat.username, chat.id)
+        file_path = pathlib.Path(f'tander/{abs(chat.id)}')
         if file_path.is_file():
             diff = datetime.now(UTC) - datetime.fromtimestamp(file_path.stat().st_mtime, UTC)
+            logger.debug("В последний раз это было %s назад", diff)
             if diff.days > 0:
                 file_path.touch()
                 await update.message.reply_text(f"Дней без упоминания Тандера: {diff.days}")
         else:
+            logger.debug("Включён Тандер для группы %s (%s)", chat.username, chat.id)
             file_path.touch()
