af91e5f8534a81c21a144ab9f968db324f479855
TyVik <tyvik8@gmail.com>
2024-01-29 21:25:35 +0300

    Change mentions

--- krddevbot/message_formatter.py
+++ krddevbot/message_formatter.py
@@ -1,20 +1,13 @@
 from typing import Optional
 
 from telegram import User
+from telegram.helpers import escape_markdown
 
 
 def md(template: str, user: Optional[dict | User] = None, **kwargs) -> str:
     if user:
         if not isinstance(user, User):
             user = User(**user)
-        username = user.username
-        if not username:
-            username = user.mention_markdown_v2(user.first_name)
-        kwargs["username"] = f"@{username}"
+        kwargs["username"] = user.mention_markdown_v2(user.username)
 
-    return template.format(**kwargs)\
-        .replace('_', '\\_')\
-        .replace('*', '\\*')\
-        .replace('[', '\\]')\
-        .replace('.', '\\.')\
-        .replace('!', '\\!')
+    return escape_markdown(template.format(**kwargs), version=2)
