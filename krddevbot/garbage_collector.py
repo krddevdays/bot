f2f58f7dc63b6fb2c8923c9075fa2ee055d863c5
Mikhail Shatokhin <brunql@gmail.com>
2024-02-05 19:24:48 +0300

    refactor: expand params for run garbage job (chat_id and message_id instead of full message)

--- krddevbot/garbage_collector.py
+++ krddevbot/garbage_collector.py
@@ -10,13 +10,15 @@ def job(context: ContextTypes.DEFAULT_TYPE, message: Message):
         _gc_task,
         settings.GARBAGE_COLLECTOR_RUN_TASK_SECONDS,
         data={
-            "message": message,
+            "chat_id": message.chat_id,
+            "message_id": message.message_id,
         },
     )
 
 
 async def _gc_task(context: ContextTypes.DEFAULT_TYPE):
     """Remove garbage message from chat"""
-    message = context.job.data["message"]
+    chat_id = context.job.data["chat_id"]
+    message_id = context.job.data["message_id"]
 
-    await context.bot.delete_message(chat_id=message.chat_id, message_id=message.message_id)
+    await context.bot.delete_message(chat_id=chat_id, message_id=message_id)
