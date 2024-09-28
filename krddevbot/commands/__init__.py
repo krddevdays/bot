590ccbe497d8533037f591355680daf8bee47970
rediska <rediska@admin.com>
2024-09-28 21:11:45 +0300

    Fix parse_mode for /ping

--- krddevbot/commands/__init__.py
+++ krddevbot/commands/__init__.py
@@ -1,6 +1,7 @@
 # commands/__init__.py
 
 from telegram import Update
+from telegram.constants import ParseMode
 from telegram.ext import ContextTypes
 from ..antispam.constance import (
     HELP_MESSAGE_TEMPLATE,
@@ -12,24 +13,23 @@ from ..message_formatter import md
 async def delete_messages(context: ContextTypes.DEFAULT_TYPE) -> None:
     """Deletes messages with a command and its response."""
     chat_id = context.job.data['chat_id']
-    help_message_id = context.job.data['help_message_id']
     
-    await context.bot.delete_message(chat_id=chat_id, message_id=help_message_id)
     await context.bot.delete_message(chat_id=chat_id, message_id=context.job.data['command_message_id'])
+    await context.bot.delete_message(chat_id=chat_id, message_id=context.job.data['help_message_id'])
 
-async def command_handler(update: Update, context: ContextTypes.DEFAULT_TYPE, *, message_template: str) -> None:
+async def command_handler(update: Update, context: ContextTypes.DEFAULT_TYPE, *, message_template: str, parse_mode: str = "") -> None:
     """Unified command handler."""
     command_message = update.message
-    message = await command_message.reply_text(message_template)
+    message = await command_message.reply_text(message_template, parse_mode=parse_mode)
     
     context.job_queue.run_once(delete_messages, when=30, data={
         'chat_id': command_message.chat_id,
-        'help_message_id': message.message_id,
-        'command_message_id': command_message.message_id
+        'command_message_id': command_message.message_id,
+        'help_message_id': message.message_id
     })
 
 async def ping_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await command_handler(update=update, context=context, message_template=md("I'm alive, {username}!", user=update.effective_user))
+    await command_handler(update=update, context=context, message_template=md("I'm alive, {username}!", user=update.effective_user), parse_mode=ParseMode.MARKDOWN_V2)
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await command_handler(update=update, context=context, message_template=HELP_MESSAGE_TEMPLATE)
