2b525e93dcee3df2dceb7a4d24bce4b71f9379bb
rediska <rediska@admin.com>
2024-10-04 18:01:16 +0300

    -setting.py from .gitignore

---
+++ krddevbot/settings.py
@@ -0,0 +1,8 @@
+import os
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO")
+SENTRY_DSN = os.environ.get("SENTRY_DSN")
+LOLS_BOT_ENABLED = bool(os.environ.get("LOLS_BOT_ENABLED", "True"))
+EMOJI_TIMEOUT_SECONDS = int(os.environ.get("EMOJI_TIMEOUT_SECONDS", "60"))
+GARBAGE_MESSAGE_TIMEOUT_SECONDS = int(os.environ.get("GARBAGE_MESSAGE_TIMEOUT_SECONDS", "30"))
